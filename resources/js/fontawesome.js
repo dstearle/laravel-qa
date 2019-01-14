78980b4f2d6f3cd9b7983c37702e8349fdda46b2
dstearle <dallas.st.earle@gmail.com>
2019-01-14 12:59:22 -0800

    Add vote controls to question and answer items

---
+++ resources/js/fontawesome.js
@@ -0,0 +1,7 @@
+import fontaweseome from  '@fortawesome/fontawesome-free/js/fontawesome';
+import faCaretUp from '@fortawesome/free-solid-svg-icons/faCaretUp';
+import faCaretDown from '@fortawesome/free-solid-svg-icons/faCaretDown';
+import faCheck from '@fortawesome/free-solid-svg-icons/faCheck';
+import faStar from '@fortawesome/free-solid-svg-icons/faStar';
+
+fontaweseome.library.add([faCaretUp, faCaretDown, faCheck, faStar]);
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -10,59 +10,102 @@
 
             <div class="card">
 
-                <div class="card-header">
+                <div class="card-body">
 
-                    <div class="d-flex align-items-center">
+                    <div class="card-title">
 
-                        <h1>{{ $question->title }}</h1>
+                        <div class="d-flex align-items-center">
 
-                        <div class="ml-auto">
+                            <h1>{{ $question->title }}</h1>
 
-                            <a href=" {{ route('questions.index') }} " class="btn btn-outline-secondary">Back to all Questions</a>
+                            <div class="ml-auto">
+
+                                <a href=" {{ route('questions.index') }} " class="btn btn-outline-secondary">Back to all Questions</a>
+
+                            </div>
 
                         </div>
 
                     </div>
-                    
-                </div>
 
-                <div class="card-body">
+                    <hr>
 
-                    {!! $question->body_html !!}
+                    <div class="media">
 
-                    {{-- Shows the name of the author for a question and date created --}}
-                                <div class="float-right">
+                        <div class="d-flex flex-column vote-controls">
 
-                                    {{-- Date the question was created--}}
-                                    <span class="text-muted">Created {{ $question->created_date }}</span>
+                            <a href="" title="This question is useful" class="vote-up">
 
-                                    {{-- User avatar and name --}}
-                                    <div class="media mt-2">
+                                <i class="btn btn-outline-success">/\</i>
 
-                                        <a href="{{ $question->user->url }}" class="pr-2">
+                            </a>
 
-                                            <img src="{{ $question->user->avatar }}">
+                            <span class="votes-count">1230</span>
+
+                            <a href="" title="This question is not useful" class="vote-down off">
+
+                                <i class="btn btn-outline-danger">\/</i>
+
+                            </a>
+
+                            <a href="" title="Click to mark as favorite question (Click again to undo)" class="favorite mt-2 favorited">
+                                
+                                <i class="btn btn-outline-warning">*</i>
+
+                                <span class="favorites-count">123</span>
+
+                            </a>
+                        
+                        </div>
+
+                        <div class="media-body">
+
+                            {!! $question->body_html !!}
+
+                            {{-- Shows the name of the author for a question and date created --}}
+                            <div class="float-right">
+
+                                {{-- Date the question was created--}}
+                                <span class="text-muted">Created {{ $question->created_date }}</span>
+
+                                {{-- User avatar and name --}}
+                                <div class="media mt-2">
+
+                                    <a href="{{ $question->user->url }}" class="pr-2">
+
+                                        <img src="{{ $question->user->avatar }}">
+
+                                    </a>
+
+                                    <div class="media-body mt-1">
+
+                                        <a href="{{ $question->user->url }}">
+
+                                            {{ $question->user->name }}
 
                                         </a>
-
-                                        <div class="media-body mt-1">
-
-                                            <a href="{{ $question->user->url }}">
-
-                                                {{ $question->user->name }}
-                                        
-                                            </a>
-
-                                        </div>
 
                                     </div>
 
                                 </div>
 
+                            </div>
+
+                        </div>
+
+                    </div>
+                
                 </div>
+
             </div>
+
         </div>
+        
     </div>
+
+
+
+
 
     <div class="row mt-4">
 
@@ -84,36 +127,60 @@
                     {{-- Populates the available answers for a particular question --}}
                     @foreach ($question->answers as $answer)
 
-                        <div class="media">
+                    <div class="media">
 
-                            <div class="media-body">
+                        <div class="d-flex flex-column vote-controls">
 
-                                {!! $answer->body_html !!}
+                            <a href="" title="This answer is useful" class="vote-up">
 
-                                {{-- Shows the name of the author for each answer and date created --}}
-                                <div class="float-right">
+                                <i class="btn btn-outline-success">/\</i>
 
-                                    {{-- Date the answer was created--}}
-                                    <span class="text-muted">Answered {{ $answer->created_date }}</span>
+                            </a>
 
-                                    {{-- User avatar and name --}}
-                                    <div class="media mt-2">
+                            <span class="votes-count">1230</span>
 
-                                        <a href="{{ $answer->user->url }}" class="pr-2">
+                            <a href="" title="This answer is not useful" class="vote-down off">
 
-                                            <img src="{{ $answer->user->avatar }}">
+                                <i class="btn btn-outline-danger">\/</i>
 
-                                        </a>
+                            </a>
 
-                                        <div class="media-body mt-1">
+                            <a href="" title="Mark this answer as best answer" class="vote-accepted mt-2">
+                                
+                                <i class="btn btn-outline-warning">$</i>
 
-                                            <a href="{{ $answer->user->url }}">
+                                <span class="favorites-count">123</span>
 
-                                                {{ $answer->user->name }}
+                            </a>
+
+                        </div>
+
+                        <div class="media-body">
+
+                            {!! $answer->body_html !!}
+
+                            {{-- Shows the name of the author for each answer and date created --}}
+                            <div class="float-right">
+
+                                {{-- Date the answer was created--}}
+                                <span class="text-muted">Answered {{ $answer->created_date }}</span>
+
+                                {{-- User avatar and name --}}
+                                <div class="media mt-2">
+
+                                    <a href="{{ $answer->user->url }}" class="pr-2">
+
+                                        <img src="{{ $answer->user->avatar }}">
+
+                                    </a>
+
+                                    <div class="media-body mt-1">
+
+                                        <a href="{{ $answer->user->url }}">
+
+                                            {{ $answer->user->name }}
                                         
-                                            </a>
-
-                                        </div>
+                                        </a>
 
                                     </div>
 
@@ -123,7 +190,9 @@
 
                         </div>
 
-                        <hr>
+                    </div>
+
+                    <hr>
 
                     @endforeach
 
